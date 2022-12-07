c59834a13cf0d839978c1640bbf389647942708c
farmanhaider123 <97297440+farmanhaider123@users.noreply.github.com>
2022-12-07 17:03:14 +0530

    Update app.js

--- Assignment_5 _/app.js
+++ Assignment_5 _/app.js
@@ -24,7 +24,7 @@ let transporter = nodemailer.createTransport({
   },
 });
 mongoose
-  .connect("mongodb+srv://farman:farman@cluster0.wumt7.mongodb.net/test/authmongo")
+  .connect("mongodb+srv://farman:<password>@cluster0.wumt7.mongodb.net/test")
   .then((res) => console.log("MongoDB Connected"))
   .catch((err) => console.log("Error : " + err));
 //end
